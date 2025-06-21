a183a3bf8e1e18915668d1adbcb0e8def2f7743d
KazikKluz <kazik.kluz@gmail.com>
2025-06-21 10:40:29 +0100

    feat: add VPC and subnets

--- variables.tf
+++ variables.tf
@@ -15,3 +15,51 @@ variable "account_id" {
   type        = string
   default     = "475822813012"
 }
+
+variable "project" {
+  description = "Name of the project"
+  type        = string
+  default     = "AWSDevOps"
+}
+
+variable "environment" {
+  description = "Name of the environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the entire VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR blok for the Public Subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR blok for the Public Subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR blok for the Private Subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR blok for the Private Subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "availability_zones" {
+  description = "set of VPC Availability Zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
