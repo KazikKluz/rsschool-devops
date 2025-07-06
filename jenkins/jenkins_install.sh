75a962e9ffd4f9b6d6efb6b0305eb1a541cf9557
KazikKluz <kazik.kluz@gmail.com>
2025-07-06 23:31:35 +0100

    fix: helm permissions

--- jenkins/jenkins_install.sh
+++ jenkins/jenkins_install.sh
@@ -37,8 +37,8 @@ kubectl apply -f jenkins-volume.yaml
 # Create Configuration for Jenkins
 curl https://raw.githubusercontent.com/KazikKluz/rsschool-devops/refs/heads/Task-4/jenkins/jenkins-values.yaml >~/jenkins-values.yaml
 
-sudo helm repo add jenkinsci https://charts.jenkins.io
-sudo helm repo update
-sudo helm search repo jenkinsci
+helm repo add jenkinsci https://charts.jenkins.io
+helm repo update
+helm search repo jenkinsci
 chart=jenkinsci/jenkins
-sudo helm install jenkins -n jenkins -f jenkins-values.yaml $chart
+helm install jenkins -n jenkins -f jenkins-values.yaml $chart
