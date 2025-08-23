1d6ce07ec230f7d81df445e01034d817daf3f127
KazikKluz <kazik.kluz@gmail.com>
2025-08-23 17:38:15 +0100

    fix: remove service account command leftovers

--- jenkins/jenkins_install.sh
+++ jenkins/jenkins_install.sh
@@ -33,7 +33,8 @@ kubectl apply -f jenkins-volume.yaml
 
 #Create Service Account for Jenkins
 #curl https://raw.githubusercontent.com/jenkins-infra/jenkins.io/master/content/doc/tutorials/kubernetes/installing-jenkins-on-kubernetes/jenkins-sa.yaml >~/jenkins-sa.yaml
-kubectl apply -f jenkins-sa.yaml
+#kubectl apply -f jenkins-sa.yaml
+
 #Create Configuration for Jenkins
 curl https://raw.githubusercontent.com/KazikKluz/rsschool-devops/refs/heads/Task-4/jenkins/jenkins-values.yaml >~/jenkins-values.yaml
 
